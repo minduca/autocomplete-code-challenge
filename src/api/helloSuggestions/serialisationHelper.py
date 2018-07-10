8d7f736711f02a0cba1b97fac6b0c4ebc79c6694
Arthur Minduca <arthur.minduca@gmail.com>
2018-07-09 23:41:59 -0400

    comment added

--- src/api/helloSuggestions/serialisationHelper.py
+++ src/api/helloSuggestions/serialisationHelper.py
@@ -1,5 +1,9 @@
 import json
 
+# I found a well stablished serializer for python called Marshmallow.  However,
+# since the problem don't have a lot of requirements I chose not to add
+# unecessary dependencies just for this.  All it does is
+# to converts a complex object into a python object that is serializable.
 def toJson(obj):
     return json.loads(json.dumps(obj, default=lambda o: o.__dict__, sort_keys=True, indent=4))
 
